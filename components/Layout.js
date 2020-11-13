75c9dcff92ec43b731d62a3a9a5bdc8e6c0a95d9
Martin Pagels <office@martinpagels.de>
2020-11-13 01:07:18 +0100

    Minor fix @import from globalstyles to index.js

--- components/Layout.js
+++ components/Layout.js
@@ -12,8 +12,14 @@ export default function Layout({ children }) {
         <title>{appTitle}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap');
+          @import
+          url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap');
+        </style>
       </Head>
-      <Navbar />
+      {/* <Navbar /> */}
       <Content>{children}</Content>
     </LayoutWrapper>
   )
