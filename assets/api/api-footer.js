769440a1ef2e0e0e8c25a4d0463be17de35ab58b
Martin Pagels <office@martinpagels.de>
2020-11-14 00:57:27 +0100

    Add api-footer

---
+++ assets/api/api-footer.js
@@ -0,0 +1,43 @@
+const footer = [
+  {
+    title: 'Sprechzeiten:',
+    info: [
+      'Mo - Do: 07:00 - 19:00',
+      'Fr: 07:00 - 18:00',
+      'Termine nach Vereinbarung',
+    ],
+  },
+  {
+    title: 'Kontakt:',
+    info: [
+      `Hartkirchener Chaussee 3
+        25469 Halstenbek`,
+      `Telefon: 04101 43 233
+          Fax: 04101 37 93 41`,
+      `Mail: info@nordlichter-halstenbek.de`,
+    ],
+  },
+  {
+    title: 'Besuchen Sie uns auf Facebook!',
+    info: ['facebook logo'],
+  },
+  {
+    title: 'Impressum',
+    info: [
+      `Die Nordlichter - Praxis für Physiotherapie
+        Hartkirchener Chaussee 3
+        25469 Halstenbek`,
+      `Inhaber & Geschäftsführer:
+        Torben Petersen-Lund`,
+
+      `Tel: 04101 432 33
+        Fax: 04101 37 93 41`,
+
+      `Mail: info@nordlichter-halstenbek.de`,
+      `www.nordlichter-halstenbek.de`,
+      ` IBAN: DE74 2219 1405 0079 8046 90`,
+    ],
+  },
+]
+
+export default footer
