01643ad09707ffb6c2430092c3770dbe54d51d5d
Martin Pagels <office@martinpagels.de>
2020-11-17 14:46:49 +0100

    Add first elements to pages/kontakt

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -1,5 +1,17 @@
-import Layout from "../components/Layout";
+import Layout from '../components/Layout'
+import kontakt from '../assets/api/api-kontakt'
+import HeadlineSvgInfo from '../components/HeadlineSvgInfo/HeadlineSvgInfo'
+import Logo from '../components/Logo/Logo'
 
 export default function Kontakt() {
-  return <Layout>Kontakt</Layout>;
+  return (
+    <Layout>
+      <Logo />
+      {kontakt.map((element) => (
+        <HeadlineSvgInfo headline={element.headline} info={element.info}>
+          {element.svg}
+        </HeadlineSvgInfo>
+      ))}
+    </Layout>
+  )
 }
