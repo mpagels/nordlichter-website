a00db64d30ce34fbf2679f5c44df30e413e4b793
Martin Pagels <office@martinpagels.de>
2020-11-17 15:33:54 +0100

    Add first simple react-hook-form example

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -2,8 +2,12 @@ import Layout from '../components/Layout'
 import kontakt from '../assets/api/api-kontakt'
 import HeadlineSvgInfo from '../components/HeadlineSvgInfo/HeadlineSvgInfo'
 import Logo from '../components/Logo/Logo'
+import { useForm } from 'react-hook-form'
 
 export default function Kontakt() {
+  const { register, handleSubmit, watch, errors } = useForm()
+  const onSubmit = (data) => console.log(data)
+
   return (
     <Layout>
       <Logo />
@@ -12,6 +16,17 @@ export default function Kontakt() {
           {element.svg}
         </HeadlineSvgInfo>
       ))}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input name="example" defaultValue="test" ref={register} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input name="exampleRequired" ref={register({ required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
     </Layout>
   )
 }
