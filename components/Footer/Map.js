36c8f91c990a9b98a73aa7fc2d11545c58e00bdc
Martin <office@martinpagels.de>
2021-03-11 00:38:10 +0100

    Minor fix

--- components/Footer/Map.js
+++ components/Footer/Map.js
@@ -2,10 +2,10 @@ export default function Map() {
   return (
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2365.897671469547!2d9.83909276725011!3d53.63096439681574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sde!2sde!4v1605314270775!5m2!1sde!2sde"
-      frameborder="0"
-      allowfullscreen=""
+      frameBorder="0"
+      allowFullScreen=""
       aria-hidden="false"
-      tabindex="0"
+      tabIndex="0"
       width="100%"
       height="350"
     ></iframe>
