c68a7addccd98b54bcf57d49f3d74b3367679ace
Martin Pagels <office@martinpagels.de>
2021-04-07 17:27:47 +0200

    Make footer responsive

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,30 +3,51 @@ import styled from 'styled-components'
 import FooterContentArea from './FooterContentArea'
 import Map from './Map'
 import Link from 'next/link'
+import { useMediaQuery } from 'react-responsive'
 
 export default function Footer() {
+  const isDesktop = useMediaQuery({ query: '(max-width: 841px)' })
   return (
     <Wrapper>
-      {footer.map((infos, index) => (
-        <div key={`footerContent_${index}`}>
-          <FooterContentArea {...infos} />
-          <Line />
-        </div>
-      ))}
-      <Link href="/datenschutz">
-        <StyledLink>Datenschutz</StyledLink>
-      </Link>
-      <Map />
+      <TextWrapper>
+        {footer.map((infos, index) => (
+          <div key={`footerContent_${index}`}>
+            <FooterContentArea {...infos} />
+            {isDesktop && <Line />}
+          </div>
+        ))}
+      </TextWrapper>
+      <LastWrapper>
+        <Link href="/datenschutz">
+          <StyledLink>Datenschutz</StyledLink>
+        </Link>
+        <Map />
+      </LastWrapper>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.footer`
   background-color: var(--footer-backgroundColor);
   color: var(--footer-font-color-lightgrey);
   text-align: center;
   padding-top: 95px;
   padding-bottom: 0;
+`
+const TextWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-wrap: wrap;
+
+  @media (min-width: 841px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto auto;
+  }
+`
+
+const LastWrapper = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
