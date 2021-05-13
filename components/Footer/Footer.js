a68e9bd398b55d91743007a24bf0e6ed322f2229
Martin Pagels <54703643+mpagels@users.noreply.github.com>
2021-05-13 23:27:10 +0200

    Add rezension link to footer (#72)
    * Add rezension link to footer
    
    * Update components/Footer/Footer.js
    
    Co-authored-by: Sören <67868341+soerenstein@users.noreply.github.com>
    
    * Update components/Footer/Footer.js
    
    Co-authored-by: Sören <67868341+soerenstein@users.noreply.github.com>
    
    * Fix typo
    
    Co-authored-by: Sören <67868341+soerenstein@users.noreply.github.com>

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,12 +3,37 @@ import styled from 'styled-components'
 import FooterContentArea from './FooterContentArea'
 import Map from './Map'
 import Link from 'next/link'
+import GoogleLogoPNG from '../../assets/png/footer/google-logo.png'
 import { useMediaQuery } from 'react-responsive'
 
 export default function Footer() {
   const isDesktop = useMediaQuery({ query: '(max-width: 841px)' })
   return (
     <Wrapper>
+      <GoogleRezension>
+        <p>
+          Waren Sie mit unser Leistung zufrieden oder haben Sie Vorschläge, wie
+          wir uns verbessern können? Dann klicken Sie auf das{' '}
+          <a
+            style={{ color: 'white' }}
+            href="https://www.google.com/search?q=nordlichter+halsenbek#lrd=0x47b18128d7d26c01:0x975243cc198cbb69,1,,,"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Google Logo
+          </a>{' '}
+          und schreiben Sie uns gerne eine Rezension. Wir freuen uns über
+          konstruktive Kritik und positive Rückmeldungen. Herzlichen Dank!
+        </p>
+        <a
+          href="https://www.google.com/search?q=nordlichter+halsenbek#lrd=0x47b18128d7d26c01:0x975243cc198cbb69,1,,,"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GoogleLogo src={GoogleLogoPNG} alt="google-logo" />{' '}
+        </a>
+      </GoogleRezension>
+
       <TextWrapper>
         {footer.map((infos, index) => (
           <div key={`footerContent_${index}`}>
@@ -21,18 +46,35 @@ export default function Footer() {
         <Link href="/datenschutz">
           <StyledLink>Datenschutz</StyledLink>
         </Link>
-        <Map />
       </LastWrapper>
+      <Map />
     </Wrapper>
   )
 }
+
+const GoogleLogo = styled.img`
+  height: 60px;
+`
+
+const GoogleRezension = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 880px;
+  margin-bottom: 40px;
+  padding: 0 20px;
+`
 
 const Wrapper = styled.footer`
   background-color: var(--footer-backgroundColor);
   color: var(--footer-font-color-lightgrey);
   text-align: center;
-  padding-top: 95px;
+  padding-top: 40px;
   padding-bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
 const TextWrapper = styled.section`
   display: flex;
