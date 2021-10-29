86ed5b66e4e3155e15254bb458349e177bc1fcdd
Martin Pagels <office@martinpagels.de>
2021-10-29 22:22:39 +0200

    Add button to TextSvgText

--- components/Pages/Leistungen.js
+++ components/Pages/Leistungen.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import BottomSvg from '../../assets/svgs/leistungen/begruessung-ende.svg'
 import CallToAction from '../CallToAction/CallToAction'
+import Link from 'next/link'
 import ContactForm from '../ContactForm/ContactForm'
 import leistungen from '../../assets/api/api-leistungen'
 import TextSvgText from '../TextSvgText/TextSvgText'
@@ -18,7 +19,16 @@ export default function LeistungenPage() {
         <span>Gesundheit</span>.
       </AboutUs>
       {leistungen.infoTexts.map(({ firstText, svg, secondText }, index) => (
-        <TextSvgText key={index} firstText={firstText} secondText={secondText}>
+        <TextSvgText
+          key={index}
+          firstText={firstText}
+          secondText={secondText}
+          button={
+            <Link href="#contactForm" passHref>
+              <StyledLink>Jetzt einen Termin vereinbaren!</StyledLink>
+            </Link>
+          }
+        >
           {svg}
         </TextSvgText>
       ))}
@@ -78,4 +88,16 @@ const Line = styled.hr`
   width: 50%;
   margin: 40px;
   height: 0.1px;
+`
+const StyledLink = styled.a`
+  all: unset;
+  background-color: #15bd77;
+  padding: 15px;
+  border-radius: 10px;
+  color: whitesmoke;
+  cursor: pointer;
+  box-shadow: 0 0.5px 2.2px rgba(0, 0, 0, 0.045),
+    0 1.3px 5.3px rgba(0, 0, 0, 0.065), 0 2.4px 10px rgba(0, 0, 0, 0.08),
+    0 4.2px 17.9px rgba(0, 0, 0, 0.095), 0 7.9px 33.4px rgba(0, 0, 0, 0.115),
+    0 19px 80px rgba(0, 0, 0, 0.16);
 `
