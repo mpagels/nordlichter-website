dd089e822299b2f3392cd14b8163a9cac771984f
Martin Pagels <office@martinpagels.de>
2021-11-10 12:54:59 +0100

    Add google font roboto 100

--- components/Layout.js
+++ components/Layout.js
@@ -12,6 +12,12 @@ export default function Layout({ children }) {
         <title>{appTitle}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Background />
       <Navbar2 />
