9eba10d2ee3540097102729657a6ba9b2958ca41
Martin <office@martinpagels.de>
2021-12-02 12:35:39 +0100

    Switch to buttonToContact component

--- components/Pages/Leistungen.js
+++ components/Pages/Leistungen.js
@@ -6,14 +6,13 @@ import ContactForm from '../ContactForm/ContactForm'
 import leistungen from '../../assets/api/api-leistungen'
 import TextSvgText from '../TextSvgText/TextSvgText'
 import TrustElements from '../TrustElements/TrustElements'
+import ButtonToContactForm from '../ButtonToContactForm/ButtonToContactForm'
 
 export default function LeistungenPage() {
   return (
     <Wrapper>
       <Headline>Herzlich Willkommen bei den Nordlichtern</Headline>
-      <Link href="#contactForm" passHref>
-        <StyledLink>Jetzt einen Termin vereinbaren!</StyledLink>
-      </Link>
+      <ButtonToContactForm />
       <AboutUs>
         Ihre <span>Ansprechpartner</span> in Halstenbek rund um das Thema
         Physiotherapie, Prävention und Gesundheit.
@@ -76,23 +75,3 @@ const bottomSvgStyle = {
   width: '350px',
   height: '100%',
 }
-
-const Line = styled.hr`
-  color: var(--line-color-lightgrey);
-  width: 50%;
-  margin: 40px;
-  height: 0.1px;
-`
-const StyledLink = styled.a`
-  all: unset;
-  background-color: #15bd77;
-  padding: 15px;
-  margin-bottom: 30px;
-  border-radius: 10px;
-  color: whitesmoke;
-  cursor: pointer;
-  box-shadow: 0 0.5px 2.2px rgba(0, 0, 0, 0.045),
-    0 1.3px 5.3px rgba(0, 0, 0, 0.065), 0 2.4px 10px rgba(0, 0, 0, 0.08),
-    0 4.2px 17.9px rgba(0, 0, 0, 0.095), 0 7.9px 33.4px rgba(0, 0, 0, 0.115),
-    0 19px 80px rgba(0, 0, 0, 0.16);
-`
