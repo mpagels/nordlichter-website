3f66972aef1c7995d8a9f56b110f8611c2aa12c7
Martin <office@martinpagels.de>
2022-11-25 22:44:27 +0100

    website has now a blog

--- pages/blog/[[...slug]].js
+++ pages/blog/[[...slug]].js
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import ReactMarkdown from 'react-markdown'
-import styles from './Home.module.css'
 import styled from 'styled-components'
 
 import dayjs from 'dayjs'
@@ -13,17 +12,6 @@ import Layout from '../../components/Layout'
 dayjs.locale('de')
 
 dayjs.extend(relativeTime)
-
-const BlogOverallHeader = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-`
-
-const BlogHeaderInfoText = styled.p`
-  max-width: 480px;
-`
 
 export default function Blog({ data, mainPage }) {
   const router = useRouter()
@@ -38,79 +26,87 @@ export default function Blog({ data, mainPage }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <div className={styles.container}>
-          <main className={styles.main}>
-            {data.data.length === 0 ? (
-              <div>
-                <div>Keine Blogeinträge gefunden</div>
-              </div>
-            ) : mainPage ? (
-              <>
-                <BlogOverallHeader>
-                  <h2>
-                    Die Nordlichter <br />
-                    Community
-                  </h2>
-                  <BlogHeaderInfoText>
-                    Willkommen in der Nordlichter Community – dem Ort für
-                    informativen Physio-Austausch. Hier bekommst du alle News,
-                    rund um die Nordlichter: Termine, Events, Physio-Knowledge,
-                    sowie Tipps und Tricks von unserem Team, damit du gesund
-                    bleibst. Viel Spaß!
-                  </BlogHeaderInfoText>
-                </BlogOverallHeader>
-                <ul className={styles.blogList}>
-                  {data.data.map((blogEntry) => {
-                    return (
-                      <li key={blogEntry.id}>
-                        <h2>{blogEntry.attributes.Title}</h2>
-                        <Image
-                          height="200"
-                          width="352"
-                          src={`http://localhost:1337${blogEntry.attributes.Media.data.attributes.formats.medium.url}`}
-                          alt=""
-                        />
-                        <h3 className={styles.autor}>
-                          Autor: {blogEntry.attributes.Verfasser}
-                        </h3>
-                        <h4 className={styles.createdDate}>
+        <Wrapper>
+          {data.data.length === 0 ? (
+            <div>
+              <div>Keine Blogeinträge gefunden</div>
+            </div>
+          ) : mainPage ? (
+            <>
+              <BlogOverallHeader>
+                <h2>
+                  Die Nordlichter <br />
+                  Community
+                </h2>
+                <BlogHeaderInfoText>
+                  Willkommen in der Nordlichter Community – dem Ort für
+                  informativen Physio-Austausch. Hier bekommst du alle News,
+                  rund um die Nordlichter: Termine, Events, Physio-Knowledge,
+                  sowie Tipps und Tricks von unserem Team, damit du gesund
+                  bleibst. Viel Spaß!
+                </BlogHeaderInfoText>
+              </BlogOverallHeader>
+              <BlogList>
+                {data.data.map((blogEntry) => {
+                  return (
+                    <BlogListItem key={blogEntry.id}>
+                      <div>
+                        <BlogTitle>{blogEntry.attributes.Title}</BlogTitle>
+                        <ImageSizeWrapper>
+                          <Image
+                            height="200"
+                            width="352"
+                            src={`http://localhost:1337${blogEntry.attributes.Media.data.attributes.formats.medium.url}`}
+                            alt=""
+                          />
+                        </ImageSizeWrapper>
+                      </div>
+                      <BlogItemDetails>
+                        <Author>Autor: {blogEntry.attributes.Verfasser}</Author>
+                        <CreatedAt>
                           Erstellt{' '}
                           {dayjs(
                             blogEntry.attributes.Erstellungszeitunkt
                           ).fromNow()}
-                        </h4>
-                        <p>{blogEntry.attributes.Preview}</p>
+                        </CreatedAt>
+                        <PreviewText>
+                          {blogEntry.attributes.Preview}
+                        </PreviewText>
                         <Link href={blogEntry.attributes.slug}>
                           Mehr lesen...
                         </Link>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </>
-            ) : (
-              <div className={styles.fullBlogEntry}>
-                <Image
-                  height="512"
-                  width="900"
-                  src={`http://localhost:1337${data.data[0].attributes.Media.data.attributes.formats.medium.url}`}
-                  alt=""
-                />
-                <h3 className={styles.autor}>
-                  Autor: {data.data[0].attributes.Verfasser}
-                </h3>
-                <h4 className={styles.createdDate}>
-                  Erstellt{' '}
-                  {dayjs(data.data[0].attributes.Erstellungszeitunkt).fromNow()}
-                </h4>
-                <ReactMarkdown>{data.data[0].attributes.Text}</ReactMarkdown>
-                <button type="button" onClick={() => router.back()}>
-                  Click here to go back
-                </button>
-              </div>
-            )}
-          </main>
-        </div>
+                      </BlogItemDetails>
+                    </BlogListItem>
+                  )
+                })}
+              </BlogList>
+            </>
+          ) : (
+            <FullBlogEntry>
+              <LinkWrapper>
+                <Link href="/blog">
+                  <a>Zurück zum Blog</a>
+                </Link>
+              </LinkWrapper>
+              <BlogTitle>{data.data[0].attributes.Title}</BlogTitle>
+              <Image
+                height="512"
+                width="900"
+                src={`http://localhost:1337${data.data[0].attributes.Media.data.attributes.formats.medium.url}`}
+                alt=""
+              />
+              <Author>Autor: {data.data[0].attributes.Verfasser}</Author>
+              <CreatedAt>
+                Erstellt{' '}
+                {dayjs(data.data[0].attributes.Erstellungszeitunkt).fromNow()}
+              </CreatedAt>
+              <ReactMarkdown>{data.data[0].attributes.Text}</ReactMarkdown>
+              <Link href="/blog">
+                <a>Zurück zum Blog</a>
+              </Link>
+            </FullBlogEntry>
+          )}
+        </Wrapper>
       </Layout>
     </>
   )
@@ -140,3 +136,110 @@ export async function getServerSideProps(ctx) {
     }
   }
 }
+
+const Author = styled.h3`
+  font-size: 0.8em;
+  margin-bottom: 0;
+`
+
+const BlogHeaderInfoText = styled.p`
+  max-width: 480px;
+`
+
+const BlogOverallHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 880px;
+
+  @media (max-width: 841px) {
+    flex-direction: column;
+    max-width: auto;
+    padding: 0 1em;
+    max-width: 480px;
+  }
+`
+
+const BlogList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(2, 352px);
+  list-style: none;
+  gap: 30px;
+  padding: 0;
+
+  @media (max-width: 841px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 0 1em;
+    max-width: 480px;
+  }
+`
+
+const BlogItemDetails = styled.div`
+  & > h2 {
+    display: none;
+    @media (max-width: 841px) {
+      display: inline;
+    }
+  }
+`
+
+const BlogListItem = styled.li`
+  @media (max-width: 841px) {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+const BlogTitle = styled.h2`
+  @media (max-width: 841px) {
+    font-size: 1.1em;
+  }
+`
+const CreatedAt = styled.h4`
+  font-size: 0.8em;
+  margin-top: 5px;
+`
+
+const FullBlogEntry = styled.div`
+  padding: 0 10em;
+
+  & > a {
+    margin: 0 0 200px 0;
+  }
+`
+
+const ImageSizeWrapper = styled.div`
+  @media (max-width: 841px) {
+    display: flex;
+    justify-content: center;
+    width: 150px;
+  }
+`
+
+const LinkWrapper = styled.div`
+  margin: 0 0 12px 0;
+  font-size: 0.8em;
+`
+
+const PreviewText = styled.p`
+  @media (max-width: 841px) {
+    font-size: 0.9em;
+  }
+`
+
+const Wrapper = styled.div`
+  padding: 4rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 0 2rem 0;
+  @media (max-width: 841px) {
+    padding: 0 0;
+  }
+`
